224863f072472465db358f644ee2daae220b0902
CyJItanCyJIeiman <125134296+CyJItanCyJIeiman@users.noreply.github.com>
2023-03-04 17:56:03 +0300

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+c={1,1,1,1,1,1,1,1,1,1,11,1,11,1}
+print(c)
+
+
+# In[6]:
+
+
+a={1,2,3,4,5}
+b={4,5,6,7,8}
+d=b-a
+print(d)
+
+
+# In[9]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[25]:
+
+
+
+
+
+# In[26]:
+
+
+# БУМБЕР ТУ 
+
+samle.append('Pank','Yellow')
+samle.append('Red')
+
+
+# In[ ]:
+
+
+
+
+
+# In[28]:
+
+
+sample={3,25,1221}
+saple={3,25}
+v=sample-saple
+s=[v]
+print(s)
+print(type(s))
+
+
+# In[33]:
+
+
+a=['Black','White','Green','Orange','Blue']
+for i in a:
+    print("Цвет: "+i)
+
+
+# In[ ]:
+
+
+
+
